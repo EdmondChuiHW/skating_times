b98b20fa8770feb990f1b3920335683cfb8e59f5
Edmond Chui <chui.hinwai@gmail.com>
2018-03-04 17:29:44 -0700

    +added model

---
+++ lib/skating_service.dart
@@ -0,0 +1,15 @@
+class ImmutableSkatingSession {
+  final DateTime startTime;
+  final DateTime endTime;
+
+  final String displayTitle;
+  final String locationDisplayName;
+
+  final String locationAddress;
+
+  const ImmutableSkatingSession(this.startTime,
+      this.endTime,
+      this.displayTitle,
+      this.locationDisplayName,
+      this.locationAddress,);
+}
